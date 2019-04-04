7499e149b609b9ebaf0c27669fdae82dad609495
SimasDei <simondeiman@gmail.com>
2019-04-04 20:59:40 +0300

    create second layout

---
+++ src/layouts/LayoutTwo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Layout Too</h1>
+    <slot/>
+    <LatestPost v-if="latest"/>
+  </div>
+</template>
+
+<script>
+import LatestPosts from "../components/LatestPosts.vue";
+export default {
+  components: {
+    LatestPosts
+  },
+  props: ["latest"]
+};
+</script>
--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -1,6 +1,15 @@
 <template>
-  <Layout>
+  <LayoutTwo :latest="false">
     <h1>Posts</h1>
-  </Layout>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque, incidunt?</p>
+  </LayoutTwo>
 </template>
 
+<script>
+import LayoutTwo from "../layouts/LayoutTwo";
+export default {
+  components: {
+    LayoutTwo
+  }
+};
+</script>
